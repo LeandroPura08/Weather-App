aec0bcd3ce1bb105c9e8d38872f00b87680a63ab
LeandroPura08 <thanoy08@gmail.com>
2023-10-12 00:50:17 +0800

    Will update the image

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,24 @@
 const apikey = "dac924bd21005c4e4adfa1622c829ad0";
-const api = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const inputItem = document.querySelector('.search input');
+
+document.querySelector('.search button').addEventListener('click',()=>{
+  checkWeather(inputItem.value)
+})
+
+
+
+async function checkWeather(city){
+  const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+  var data = await response.json();
+  console.log(data);
+
+
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
+  document.querySelector('.humudity').innerHTML = Math.round(data.main.humidity) +'%';
+  document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+
+}
